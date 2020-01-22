e119321056cfc58a6384dd0175e97fbaec87338c
Le Huu Phu <46106387+lhp1507@users.noreply.github.com>
2020-01-22 21:54:02 +0700

    Test Commit

---
+++ esp8266_code.ino
@@ -0,0 +1,32 @@
+/*
+ * D:\KT_MayTinh\Semester 9th DATN\Phu_Dau\Diagrams\ESP-devices.png
+ */
+ 
+/*
+ * Thanh phan:
+ *  dht11
+ *  esp8266
+ *  lcd2004 - i2c
+ *  led - res
+ *  74hc138 (demux)
+ */
+/*
+ * Chuc nang:
+ *  
+ *  
+ *  
+ *  
+ *  
+ *  
+ */
+
+ 
+void setup()
+{
+  
+}
+
+void loop()
+{
+  
+}
